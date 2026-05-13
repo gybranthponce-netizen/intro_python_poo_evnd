d9fd75e3c829dc3f69bb287ea01893dbc1d06320
gybranthponce-netizen <gybranthponce@gmail.com>
2026-05-12 18:57:59 -0600

    Primer práctica: función sumar en Python

---
+++ Funcion_sumar_Primera_Practica.py
@@ -0,0 +1,13 @@
+# Primera Practica de función en Python
+# Suma de dos números y que regresa el resultado
+
+def sumar(a, b):
+    return a + b
+
+# Un ejemplo
+x = 5
+y = 3
+resultado = sumar(x, y)
+print("La suma de", x, "y", y, "es:", resultado)
+
+#Fin
